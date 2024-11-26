8332f70b5f714fbff051a2821230711156fa0dde
Nitish0943 <ntshpthk09@gmail.com>
2024-11-26 22:48:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Scroll to a section smoothly
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  
